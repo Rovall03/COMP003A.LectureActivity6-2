221781d67bf3a0cb8a67eb055c115cd933b9ba3e
rosa ovalle <you@example.com>
2025-02-18 18:53:17 -0800

    added commments

--- COMP003A.LectureActivity6-2/Program.cs
+++ COMP003A.LectureActivity6-2/Program.cs
@@ -4,16 +4,22 @@
     {
         static void Main(string[] args)
         {
+
+            //demostrating methode overloading
             ShapeCalcuator calculator = new ShapeCalcuator();
             Console.WriteLine($"Circle Area: {calculator.CalculateArea(5)}");
             Console.WriteLine($"Rectangle Area: {calculator.CalculateArea(5,10)}");
             Console.WriteLine($"Triangle Area: {calculator.CalculateArea(5,10,true)}");
 
-
+            //demonstrating method overrriding
+            //creating a base class refrence to a derived class object for polymorphism
             Shape myCrircle = new Circle(7);
 
+
+            //creating a base class reference to a derived class object for polymorphism
             Shape myRectangle = new Rectangle(4,8);
 
+            //calling the DisplayArea method from the circle and rectangle objects
             myCrircle.DisplayArea();
             myRectangle.DisplayArea();
         
--- COMP003A.LectureActivity6-2/Rectangle.cs
+++ COMP003A.LectureActivity6-2/Rectangle.cs
@@ -6,15 +6,30 @@ using System.Threading.Tasks;
 
 namespace COMP003A.LectureActivity6_2
 {
+
+    /// <summary>
+    /// represents a rectangle with a specific area calculation
+    /// </summary>
     class Rectangle : Shape
     {
+        //Auto iplmanted properties for the length and width of the rectangle
         public double Length { get; set; }
         public double Width { get; set; }
+
+        /// <summary>
+        /// constructor fort the rectangle class
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="width"></param>
     public Rectangle(double length, double width) 
         {
             Length = length;
             Width = width;
         }
+
+        /// <summary>
+        /// displays tthe area of a rectangle
+        /// </summary>
         public override void DisplayArea()
         {
             Console.WriteLine($"Rectangle Area: {Length * Width}");
